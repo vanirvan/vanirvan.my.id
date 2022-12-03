bbee635dd7f82fb4081f48ed54778024f7182007
vanirvan <vanir1812@gmail.com>
2022-12-03 20:03:34 +0700

    using Edge Runtime

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+// Edge Runtime
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+
+import type { NextRequest } from 'next/server'
+
+export const config = {
+	runtime: 'experimental-edge',
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function (req: NextRequest) {
+	return new Response(JSON.stringify({ name: 'John Doe' }), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
 }
