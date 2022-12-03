d32ec57b028bc55ea5b3e2ae7b7111fa9d69cd96
vanirvan <vanir1812@gmail.com>
2022-12-03 18:23:05 +0700

    project's lists

---
+++ utils/data/projectList.ts
@@ -0,0 +1,13 @@
+// List of projects
+const ProjectList = [
+	{
+		thumbnail: '/assets/img/projects/project-yuiko.webp',
+		title: 'Yuiko',
+		description: 'Transform UI Design into HTML CSS Website.',
+		visit: 'https://yuiko-vanirvan.vercel.app/',
+		github: 'https://github.com/vanirvan/yuiko',
+		detail: '#',
+	},
+]
+
+export default ProjectList
