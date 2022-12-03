ceb8a8d3eade469e984101cc1959599501c7a8e9
vanirvan <vanir1812@gmail.com>
2022-12-03 18:20:27 +0700

    create: Footer (disabled in app)

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+		<div
+			className={`w-full border-t-2 border-[#65c3c8] bg-white pb-20 font-fira-code sm:pb-0`}>
+			<div
+				className={`mx-auto flex max-w-xs justify-center py-8 sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-5xl`}>
+				<span className={``}>
+					Made with ♥. Powered by NextJS. Deployed on Cloudflare (edit later)
+				</span>
+			</div>
+		</div>
+	)
+}
+
+export default Footer
